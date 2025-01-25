ab2530819bc747ea6c44e08f82950e1da34e66f0
Azzam yaqoub alsaeed <3zamPlayz@gmail.com>
2025-01-25 21:20:52 +0300

    created bsae temp

--- capstone/LPS/urls.py
+++ capstone/LPS/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landingpage, name='landingpage'),
-    path('profile/', views.profilepage, name='profilepage'),
-    path('checkout/', views.checkoutpage, name='checkoutpage'),
-    path('search/', views.searchingpage, name='searchingpage'),
+    path('', views.home, name='landingpage'),
+    path('auction/', views.auction, name='auction'),
+    path('forsale/', views.forsale, name='forsale'),
 ]
