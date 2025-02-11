a584273f2f297a3a96af16415511dd939ba892e4
Azzam yaqoub alsaeed <3zamPlayz@gmail.com>
2025-02-11 21:00:32 +0300

    SIGN IN LOG IN pages

--- capstone/LPS/urls.py
+++ capstone/LPS/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import signup, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('', views.home, name='landingpage'),
     path('auction/', views.auction, name='auction'),
     path('forsale/', views.forsale, name='forsale'),
-    path('addlisting/', views.addlisting, name='addlisting')
+    path('addlisting/', views.addlisting, name='addlisting'),
+    path('signup/', signup, name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- capstone/LPS/views.py
+++ capstone/LPS/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import redirect, render
 from .forms import LicensePlateListingForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
 
 def home(request):
     return render(request, 'landingpage.html')
@@ -19,3 +23,24 @@ def addlisting(request):
     else:
         form = LicensePlateListingForm()
     return render(request, 'addlisting.html', {'form': form})
+
+# sign up view
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in after sign up
+            messages.success(request, "Registration successful!")
+            return redirect('home')  # Redirect to homepage or dashboard
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'signup.html', {'form': form})
+
+# login view
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = 'home'  # Redirect after logout
